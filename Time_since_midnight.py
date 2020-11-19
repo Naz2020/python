6d5379b3792438449b0657c7c7d6a1994c8bda23
Albedaiwi <60201228+Naz2020@users.noreply.github.com>
2020-11-19 02:47:15 -0700

    Create Time_since_midnight.py

---
+++ Time_since_midnight.py
@@ -0,0 +1,7 @@
+seconds = int(input('Enter number of seconds: '))
+print("hoursHappend")
+hoursHappend =input(seconds/3600)
+print("minsHappend")
+minsHappend = input((seconds%3600)/60)
+print("secondHappend")
+secondHappend = input((seconds%3600)%60)
