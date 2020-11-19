1fbe3556cbf627b25005b204a30577ee60efc2dc
Albedaiwi <60201228+Naz2020@users.noreply.github.com>
2020-11-19 02:28:45 -0700

    Create rock_paper_scissors.py

---
+++ rock_paper_scissors.py
@@ -0,0 +1,22 @@
+import random
+user = input("Choose one from the options [rock, paper, scissors]: ").lower()
+list = ["rock","paper","scissors"]
+computer = random.choice(list)
+if user in list:
+    print("Your Choice: " + user)
+    print("computer Choice: " + computer)
+    if user == computer:
+        print("You and computer chooses same option.")
+    elif user == "scissors" and computer == "rock":
+        print(" You loose and computer loose ")
+    elif user == "scissors" and computer == "paper":
+        print("You won and computer loose")
+    elif user == "rock" and computer == "scissors":
+        print("You won and computer loose")
+    elif user == "rock" and computer == "paper":
+        print("you loose")
+    elif user == "paper" and computer == "scissors":
+        print("You loose and computer won")
+    else:
+        print("You loose and computer won")
+
