224c5b565e067772a714d5e1ceed24e8fd8f1f77
Albedaiwi <60201228+Naz2020@users.noreply.github.com>
2020-11-19 02:53:36 -0700

    Create Rolling_dice.py

---
+++ Rolling_dice.py
@@ -0,0 +1,6 @@
+import random
+dice1 = random.randrange(1 , 6)
+dice2 = random.randrange(1 , 6)
+dice3 = str(dice1 + dice2)
+print("you rolled: " +str(dice1) + " and: " +str(dice2) )
+print("total of: " +dice3)
